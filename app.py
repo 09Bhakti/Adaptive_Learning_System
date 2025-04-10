383de5f0e6835e94cdb20bbe95911d730a1f4406
09Bhakti <132741211+09Bhakti@users.noreply.github.com>
2025-04-11 01:52:14 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+pip install streamlit
+
+
+import streamlit as st
+
+st.title("Adaptive Learning System")
+
+score = st.slider("Enter your last test score:", 0, 100)
+if st.button("Get Recommendation"):
+    if score < 50:
+        st.write("📘 Beginner Level Content Recommended")
+    elif 50 <= score < 80:
+        st.write("📗 Intermediate Level Content Recommended")
+    else:
+        st.write("📕 Advanced Level Content Recommended")
